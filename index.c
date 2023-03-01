c870bcb3a2cc89b0d15a90b57d85af8a391689c8
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2023-03-01 05:52:47 -0300

    ,

--- index.c
+++ index.c
@@ -1,23 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+    int x, fat;
     int fatorial(int x){
         if(x<=1){
             return 1;
         }else{
-            return x*fatorial(x-1);
+            return (x*fatorial(x-1));
             }
         }
 
 int main()
 {
-int fat, n;
 printf("Insira um valor para o qual deseja calcular seu fatorial: ");
-scanf("%d", &n);
+scanf("%d", &x);
+printf("Seu fatorial e: \n", + fatorial(x));
 
-for(fat = 1; n > 1; n = n - 1)
-fat = fat * n;
-
-printf("\nFatorial calculado: %d", fat);
-return 0;
 }
